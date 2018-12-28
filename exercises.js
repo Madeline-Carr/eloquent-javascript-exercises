036ffe9d080f77ea1155731c379e31d795555f80
Madeline Carr <Carrm2@vcu.edu>
2018-12-28 15:23:31 -0500

    FizzBuzz
    I did the FizzBuzz exercise and I made a new exercise where the numbers that are not divisible by 3 or 5 get logged with burp!

--- exercises.js
+++ exercises.js
@@ -3,7 +3,7 @@ function triangle() {
     let tri = "#";
     for (let number = 1; number <= 7; number++) {
         console.log(tri);
-        tri = tri + "#";
+        tri += "#";
     };
 }
 triangle();
@@ -13,8 +13,44 @@ function triangle2() {
     let tri = "#";
     while (tri.length < 7) {
         console.log(tri);
-        tri = tri + "#";
+        tri += "#";
     }
 }
 triangle2();
 
+//FizzBuzz
+function fizzBuzz() {
+    for (let i = 3; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log(i + " FizzBuzz");
+            continue;
+        }
+        if (i % 3 == 0) {
+            console.log(i + " Fizz");
+        }
+        if (i % 5 == 0) {
+            console.log(i + " Buzz");
+        }
+    }
+}
+fizzBuzz();
+
+//FizzBuzz
+function fizzBuzzBurp() {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            console.log(i + " FizzBuzz");
+            continue;
+        }
+        if (i % 3 == 0) {
+            console.log(i + " Fizz");
+        }
+        if (i % 5 == 0) {
+            console.log(i + " Buzz");
+        }
+        else {
+            console.log(i + " Burp!!!");
+        }
+    }
+}
+fizzBuzzBurp();
