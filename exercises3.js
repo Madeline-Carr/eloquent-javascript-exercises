2c7efb402faf9a64d14544d43943cf8b99c047a9
Madeline Carr <Carrm2@vcu.edu>
2019-01-01 22:02:01 -0500

    Sum of Range Bonus
    I fixed the function so it makes an array instead of just a value. I also did the bonus assignment and added a step parameter to the function.

--- exercises3.js
+++ exercises3.js
@@ -1,12 +1,14 @@
 //The sum of a range
-function sumOfRange(start, end) {
+function sumOfRange(start, end, step = 1) {
+    let arrayOfNumbers = [];
     let counter = 0;
     for (let index = start; index <= end; index++) {
-        counter += index;
+        counter += step;
+        arrayOfNumbers.push(counter);
     }
-    return counter;
+    return arrayOfNumbers;
 }
-
-console.log(sumOfRange(1, 8));
-
-//   node exercises3.js
+//Uses the default step
+console.log(sumOfRange(1, 6));
+//Has its own step
+console.log(sumOfRange(1, 6, 2));
