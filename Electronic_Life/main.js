3340c633b130c74422d773cf0ac5528dc55aef33
Madeline Carr <Carrm2@vcu.edu>
2019-01-28 23:29:46 -0500

    Electronic Life
    I started the electronic life project.

---
+++ Electronic_Life/main.js
@@ -0,0 +1,31 @@
+//Electronic Life
+var plan = {
+    "#####################################",
+    "####        ######        ############",
+    "#####       ####                 ####",
+    "###     o     ##                    #",
+    "#                  ####             #",
+    "#    ###          ##      o    ######",
+    "##    ##         ###            #####",
+    "##   ####       ###  #              #",
+    "###    #                          ###",
+    "###                                 #",
+    "###       ###  o                   ##",
+    "##       ##          #       ###  ###",
+    "###     ###        ###   ############",
+    "#####################################"
+};
+
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Vector.prototype.plus = function (other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+};
+
+var grid = [["top left", "top middle", "top right"],
+["bottom left", "bottom middle", "bottom right"]];
+console.log(grid[1][1]); //Bottom Middle
+console.log(grid[0][2]); //Top Middle
+console.log(grid[1][2]); //Bottom Right
